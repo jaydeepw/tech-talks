80355738028fa7e34a5b2ded4b1d705e75c9f8af
jay <jaydeep.w@gmail.com>
2012-08-11 11:50:07 +0530

    Notificaitons demo updated, but there are some deprecated APIs that have been used in it. Need to work on it in free time and make it working for the demo.

--- Android/Beginner/Demos/NotificationDemo/src/com/learning/notificationdemo/MainActivity.java
+++ Android/Beginner/Demos/NotificationDemo/src/com/learning/notificationdemo/MainActivity.java
@@ -2,6 +2,11 @@ package com.learning.notificationdemo;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -29,8 +34,32 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				Log.i(TAG, "#initShowNotificaiton: Button has been clicked.");
+				showNotification();
 			}
 		});
+	}
+
+	@SuppressWarnings("deprecation")
+	protected void showNotification() {
+		NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		
+		int icon = R.drawable.ic_launcher;
+		CharSequence tickerText = "Hello";
+		long when = System.currentTimeMillis();
+
+		Notification notification = new Notification(icon, tickerText, when);
+		
+		Context context = getApplicationContext();
+		CharSequence contentTitle = "My notification";
+		CharSequence contentText = "Hello World!";
+		Intent notificationIntent = new Intent(this, MyClass.class);
+		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+		notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
+		
+		private static final int HELLO_ID = 1;
+
+		mNotificationManager.notify(HELLO_ID, notification);
 	}
     
 }
