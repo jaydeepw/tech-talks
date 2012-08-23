f16fb645a417f466a37ed2518d9911ad369e875a
jay <jaydeep.w@gmail.com>
2012-08-23 23:22:45 +0530

    Notification demo is working now.

--- Android/Beginner/Demos/NotificationDemo/src/com/learning/notificationdemo/MainActivity.java
+++ Android/Beginner/Demos/NotificationDemo/src/com/learning/notificationdemo/MainActivity.java
@@ -7,6 +7,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -44,22 +45,44 @@ public class MainActivity extends Activity {
 		NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 		
 		int icon = R.drawable.ic_launcher;
-		CharSequence tickerText = "Hello";
+		CharSequence tickerText = "Hello"; 
 		long when = System.currentTimeMillis();
-
+		
+		//Android 2.3.3
 		Notification notification = new Notification(icon, tickerText, when);
 		
 		Context context = getApplicationContext();
 		CharSequence contentTitle = "My notification";
 		CharSequence contentText = "Hello World!";
-		Intent notificationIntent = new Intent(this, MyClass.class);
+		Intent notificationIntent = new Intent(this, MainActivity.class);
 		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
 
 		notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
 		
-		private static final int HELLO_ID = 1;
+		final int HELLO_ID = 1;
 
 		mNotificationManager.notify(HELLO_ID, notification);
+
+		//Android 4.0+ code.
+		/*Notification.Builder notifiBuilder = new Notification.Builder(this);
+		notifiBuilder.setTicker( tickerText );
+		notifiBuilder.setWhen( when );
+		notifiBuilder.setLargeIcon( BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher) );
+		
+		Context context = getApplicationContext();
+		CharSequence contentTitle = "My notification";
+		CharSequence contentText = "Hello World!";
+		Intent notificationIntent = new Intent(this, MainActivity.class);
+		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+		//Notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
+		notifiBuilder.setContentTitle(contentTitle);
+		notifiBuilder.setContentText(contentText);
+		notifiBuilder.setContentIntent(contentIntent);
+		
+		final int HELLO_ID = 1;
+
+		mNotificationManager.notify(HELLO_ID, notifiBuilder.getNotification() );*/
 	}
     
 }
