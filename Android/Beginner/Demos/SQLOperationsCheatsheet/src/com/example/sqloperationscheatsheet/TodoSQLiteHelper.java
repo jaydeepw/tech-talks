f995c3202b13969556d81294cdd1f9a5ed485c53
jay <jaydeep.w@gmail.com>
2013-01-05 19:45:02 +0530

    This app has accidently been create in the github pages branch. Got to move it into master branch.

--- Android/Beginner/Demos/SQLOperationsCheatsheet/src/com/example/sqloperationscheatsheet/TodoSQLiteHelper.java
+++ Android/Beginner/Demos/SQLOperationsCheatsheet/src/com/example/sqloperationscheatsheet/TodoSQLiteHelper.java
@@ -15,10 +15,10 @@ public class TodoSQLiteHelper extends SQLiteOpenHelper {
   private static final int DATABASE_VERSION = 1;
 
   // Database creation sql statement
+  // "create table todo_list (_id integer primary key autoincrement, item text not null);
   private static final String DATABASE_CREATE = "create table "
-      + TABLE_TODO_LIST + "(" + COL_ID
-      + " integer primary key autoincrement, " + COL_ITEM
-      + " text not null);";
+      + TABLE_TODO_LIST + "(" + COL_ID + " integer primary key autoincrement, " 
+		 	+ COL_ITEM + " text not null);";
 
   public TodoSQLiteHelper(Context context) {
     super(context, DATABASE_NAME, null, DATABASE_VERSION);
