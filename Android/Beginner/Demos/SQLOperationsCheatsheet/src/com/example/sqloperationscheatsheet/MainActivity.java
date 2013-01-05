bad6ad7e824ef39db38788354cca8522d30030f0
jay <jaydeep.w@gmail.com>
2013-01-05 21:30:20 +0530

    Add item has been implemented. Got to show the items that are added into the UI.

--- Android/Beginner/Demos/SQLOperationsCheatsheet/src/com/example/sqloperationscheatsheet/MainActivity.java
+++ Android/Beginner/Demos/SQLOperationsCheatsheet/src/com/example/sqloperationscheatsheet/MainActivity.java
@@ -4,15 +4,17 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
-	private static final String TAG = "MainActivity";
+	private static final String TAG = MainActivity.class.getName();
 	
 	private Button mAddItem;
 	private EditText mTodoItemET;
@@ -35,31 +37,60 @@ public class MainActivity extends Activity {
 			
 			@Override
 			public void onClick(View v) {
-				Log.d(TAG, "#addItemClicked ");
 				String todoItem = mTodoItemET.getText().toString();
+				
+				if( todoItem == null || TextUtils.isEmpty(todoItem) ) {
+					showToastMsg("Please enter a todo item to add");
+					return;	
+				}
 				
 				addItem(todoItem);
 			}
 		});
 	}
 
+	protected void showToastMsg(String msg) {
+		Toast toast = Toast.makeText(this, msg, Toast.LENGTH_SHORT);
+		toast.show();
+	}
+
 	protected void addItem(String todoItem) {
-		Log.d(TAG, "#addItem current item " + todoItem );
 		
-		TodoSQLiteHelper helper = new TodoSQLiteHelper(this);
+		TodoSQLiteHelper helper = null;
+		SQLiteDatabase db = null;
 		
-		SQLiteDatabase db = helper.getWritableDatabase();
-		
-		ContentValues values = new ContentValues();
-		
-		values.put(Constants.COL_ITEM, todoItem);
-		long rowId = db.insert(Constants.TABLE_TODO_LIST, null, values);
-		
-		// error handling
-		if( rowId == -1 ) {
-			Log.e(TAG, "#addItem Error inserting the item into the table");
-		} else
-			Log.d(TAG, "#addItem Item added successfully");
+		try {
+			helper = new TodoSQLiteHelper(this);
+			db = helper.getWritableDatabase();
+			
+			ContentValues values = new ContentValues();
+			
+			values.put(Constants.COL_ITEM, todoItem);
+			long rowId = db.insert(Constants.TABLE_TODO_LIST, null, values);
+			
+			// error handling
+			if( rowId == -1 ) {
+				Log.e(TAG, "#addItem Error inserting the item into the table");
+				showToastMsg("Error inserting the item");
+			} else {
+				showToastMsg("Item inserted successfully");
+			}	
+		} catch (Exception e) {
+			// BEST PRACTICE: its always a good practice to print the reason when an exception rises.
+			Log.i(TAG, "#addItem Exception while inserting into db. Reason: " + e.getMessage() );
+		} finally {
+			// BEST PRACTICE: Always close the cursor and DB in the 'finally' clause
+			// not doing so may let you into debugging the entire app sometime later
+			// when the project is about 2-3 year old.
+			if( db != null && db.isOpen() )
+				db.close();
+			
+			if( helper != null )
+				helper.close();
+			
+			// NOTE: The sequence of closing DB and DBHelper and cursor matters a lot.
+			// Read more about this here  http://stackoverflow.com/questions/4195089/what-does-invalid-statement-in-fillwindow-in-android-cursor-mean/8325904#8325904
+		}
 	}
     
 }
--- Android/Beginner/Demos/SQLOperationsCheatsheet/src/com/example/sqloperationscheatsheet/TodoSQLiteHelper.java
+++ Android/Beginner/Demos/SQLOperationsCheatsheet/src/com/example/sqloperationscheatsheet/TodoSQLiteHelper.java
@@ -12,7 +12,7 @@ public class TodoSQLiteHelper extends SQLiteOpenHelper {
 
   // Database creation sql statement
   // "create table todo_list (_id integer primary key autoincrement, item text not null);
-  private static final String DATABASE_CREATE = "create table "
+  private static final String CREATE_TABLE_TODO_LIST = "create table "
       + Constants.TABLE_TODO_LIST + "(" + Constants.COL_ID + " integer primary key autoincrement, " 
 		 	+ Constants.COL_ITEM + " text not null);";
 
@@ -22,14 +22,14 @@ public class TodoSQLiteHelper extends SQLiteOpenHelper {
 
   @Override
   public void onCreate(SQLiteDatabase database) {
-    database.execSQL(DATABASE_CREATE);
+    database.execSQL(CREATE_TABLE_TODO_LIST);
   }
 
   @Override
   public void onUpgrade( SQLiteDatabase db, int oldVersion, int newVersion ) {
-    Log.w(TodoSQLiteHelper.class.getName(), "Upgrading database from version " + oldVersion + " to " + newVersion + ", which will destroy all old data");
-    // db.execSQL("DROP TABLE IF EXISTS " + Constants.TABLE_TODO_LIST);
-    // onCreate(db);
+    Log.w( TodoSQLiteHelper.class.getName(), "Upgrading database from version " + oldVersion + " to " + newVersion + ", which will destroy all old data" );
+    db.execSQL("DROP TABLE IF EXISTS " + Constants.TABLE_TODO_LIST);
+    onCreate(db);
   }
 
 } 
